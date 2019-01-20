757c270c40aef60908ac70bfe314a13f66a8c697
jeffxucoder <2896608654@qq.com>
2019-01-20 21:45:51 +0800

    the modify of the hello-world .

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # hello-world
 just-test
 this file just for test the git function.
+now i have change the readmefile .
+test for this change .
+I have change the file use vim ,
+let's see what happen.
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main(){
 cout<<"this is just test for the git \n"<<endl ;
 for(int i=0 ; i<10 ; i++){
-cout << "i="<<i <<\t<<endl ;
+cout << "i="<< i <<\t<<endl ;
+cout << "the new add for the file ."<< endl ;
 }
 }
